a70edd2539856cdc9098d8dc129c5b2ff0a69c52
USER <USER@DESKTOP-DHS6L02>
2024-02-16 06:07:58 -0600

    Agregar archivos de proyecto.

---
+++ TAREA 1 PARTE 3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+{
+    //CADENA DE TEXTO IMPRIMIDA EN MAYUSCULAS
+   
+Console.WriteLine("Ingrese cualquier palabra o texto en minusculas:");
+    string text1 = Console.ReadLine();
+    text1 = text1.ToUpper();
+    Console.WriteLine("Este es su texto en minusculas:"+text1);
+}
+
+{
+    //CADENA DE TEXTO IMPRIMIDA EN MINUSCULAS
+    Console.WriteLine("Ingrese cualquier palabra o texto en mayusculas:");
+    string text1 = Console.ReadLine();
+    text1 = text1.ToLower();
+
+    Console.WriteLine("Este es su texto en MAYUSCULAS:"+text1);
+}
